3b1e81388e7d0e4caa4a96626c263eddf999d559
youjingyu <yuyoujing@hotmail.com>
2025-12-16 16:52:02 +0000

    compute global mean/var with training data only.

--- bin/main.ml
+++ bin/main.ml
@@ -12,7 +12,7 @@ let _ =
 
 
 let in_dir = Cmdargs.in_dir "-results"
-let data_folder = Option.value_exn (Cmdargs.get_string "-data")
+let data_path = Option.value_exn (Cmdargs.get_string "-data")
 
 (* -----------------------------------------
    ---- Training Arguments -----
@@ -25,7 +25,7 @@ let n = Option.value_exn (Cmdargs.get_int "-n")
 
 (* control dim *)
 let m = Option.value_exn (Cmdargs.get_int "-m")
-let mini_batch = 10
+let mini_batch = 32
 
 (* -----------------------------------------
    -- Data Read In ---
@@ -69,30 +69,31 @@ let c_init ~dim x =
 let tmax, pcs, data_train, train_batch_size, data_save_results, data_test =
   C.broadcast' (fun () ->
     (* shape [n_trials x tmax x n_channels ]*)
-    let data_full = Arr.load_npy data_folder in
-    let data_shape = Arr.shape data_full in
-    let full_batch_size = data_shape.(0) in
-    let tmax = data_shape.(1) in
-    let train_batch_size = Float.(to_int (of_int full_batch_size *. 0.8)) in
-    let data_train = Arr.get_slice [ [ 0; train_batch_size - 1 ]; []; [] ] data_full in
-    let data_test =
+    let data_train_npy = Arr.load_npy (data_path ^ "train_std.npy") in
+    let data_test_npy = Arr.load_npy (data_path ^ "test_std.npy") in
+    let data_train_shape = Arr.shape data_train_npy in
+    (* let full_batch_size = data_shape.(0) in *)
+    let train_batch_size = data_train_shape.(0)
+    and tmax = data_train_shape.(1) in
+    (* let train_batch_size = Float.(to_int (of_int full_batch_size *. 0.8)) in *)
+    (* let data_train = Arr.get_slice [ [ 0; train_batch_size - 1 ]; []; [] ] data_full in *)
+    (* let data_test =
       Arr.get_slice [ [ train_batch_size; full_batch_size - 1 ]; []; [] ] data_full
-    in
+    in *)
     print
       [%message
-        (Arr.shape data_full : int array)
-          (Arr.shape data_train : int array)
-          (Arr.shape data_test : int array)];
-    let data_train = pack_data data_train in
-    let data_test = pack_data data_test in
+        (Arr.shape data_train_npy : int array) (Arr.shape data_test_npy : int array)];
+    let data_train = pack_data data_train_npy in
+    let data_test = pack_data data_test_npy in
     let data_save_results =
-      List.permute (List.range 0 train_batch_size)
+      List.permute (List.range 0 data_train_shape.(0))
       |> List.sub ~pos:0 ~len:n_trials_save
       |> List.map ~f:(fun i -> data_train.(i))
       |> List.to_array
     in
     let data_test = truncate data_test mini_batch in
-    let pcs = c_init ~dim:n data_full in
+    let pcs = c_init ~dim:n data_train_npy in
+    print [%message "Finished computing PCs"];
     tmax, pcs, data_train, train_batch_size, data_save_results, data_test)
 
 
@@ -158,19 +159,31 @@ module M = Make_model (struct
 
 open M
 
+let reg ~(prms : Model.P.t') =
+  let z = Float.(1e-5 / of_int Int.(setup.n * setup.n)) in
+  let a = D.unpack_a ~prms:prms.dynamics in
+  let a_reg = AD.Maths.(F z * l2norm_sqr' a) in
+  match prms.dynamics.b with
+  | None -> a_reg
+  | Some b ->
+    let b_reg = AD.Maths.(F z * l2norm_sqr' b) in
+    AD.Maths.(a_reg + b_reg)
+
+
 let init_prms () =
   C.broadcast' (fun () ->
     let n = setup.n
     and m = setup.m in
     (* pin prior and prior_recog *)
-    let prior = U.init ~spatial_std:1.0 ~m () in
-    (* let prior = U.P.map (U.init ~spatial_std:1.0 ~m ()) ~f:Prms.pin in *)
-    let prior_recog = UR.init ~spatial_std:1.0 ~m () in
+    (* let prior = U.init ~spatial_std:1.0 ~m () in *)
+    let prior = U.P.map (U.init ~spatial_std:1.0 ~m ()) ~f:Prms.pin in
+    let prior_recog = prior in
     (* initialise away from margin *)
     let dynamics = D.init ~dt_over_tau:Float.(dt / tau) ~alpha:0.5 ~beta:0.5 ~n ~m () in
     (* use PCA to initialise C matrix *)
     let likelihood =
       let tmp = L.init ~scale:1. ~sigma2:1. ~n ~n_output () in
+      (* tmp *)
       { tmp with c = Prms.create (AD.pack_arr pcs) }
     in
     Model.init ~prior ~prior_recog ~dynamics ~likelihood ())
@@ -187,7 +200,7 @@ let save_results prefix prms data =
     then (
       let mu = Model.posterior_mean ~prms dat_trial in
       AA.save_txt ~out:(file (Printf.sprintf "posterior_u_%i" i)) (AD.unpack_arr mu);
-      let us, zs, os = Model.predictions ~n_samples:1 ~prms mu in
+      let us, zs, os = Model.predictions ~n_samples:100 ~prms mu in
       let process label a =
         let a = AD.unpack_arr a in
         AA.(mean ~axis:2 a @|| var ~axis:2 a)
@@ -203,7 +216,7 @@ module Optimizer = Opt.Adam.Make (Model.P)
 
 let config _k =
   Opt.Adam.
-    { learning_rate = Some 0.01
+    { learning_rate = Some 0.001
     ; epsilon = 1E-4
     ; beta1 = 0.9
     ; beta2 = 0.999
@@ -220,12 +233,12 @@ let rec iter ~k state =
   then (
     let test_loss =
       Model.elbo_no_gradient
-        ~n_samples:1
+        ~n_samples:100
         ~conv_threshold:1E-4
         (Model.P.value prms)
         data_test
     in
-    (* print [%message (k : int) (test_loss : float)]; *)
+    print [%message (k : int) (test_loss : float)];
     if C.first
     then (
       Optimizer.save ~out:(in_dir "state.bin") state;
@@ -236,7 +249,13 @@ let rec iter ~k state =
           (of_array [| Float.of_int k; test_loss |] [| 1; 2 |]));
       save_results (in_dir "final") prms data_save_results));
   let loss, g =
-    Model.elbo_gradient ~n_samples:1 ~mini_batch ~conv_threshold:1E-4 prms data_train
+    Model.elbo_gradient
+      ~n_samples:100
+      ~mini_batch
+      ~conv_threshold:1E-4
+      ~reg
+      prms
+      data_train
   in
   (if C.first
    then
@@ -274,7 +293,7 @@ let _ =
     C.broadcast' (fun () -> Misc.read_bin (in_dir "final.params.bin") |> Model.P.value)
   in
   let test_loss =
-    Model.elbo_no_gradient ~n_samples:1 ~conv_threshold:1E-4 prms_final data_test
+    Model.elbo_no_gradient ~n_samples:100 ~conv_threshold:1E-4 prms_final data_test
   in
   if C.first
   then
